c3a9574a967f81254f8c7bee9eeb3b118abcb1fa
merefield <merefield@gmail.com>
2024-11-14 17:05:29 +0000

    various install warnings about deps resolved

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import { terser } from "rollup-plugin-terser";
+import terser from '@rollup/plugin-terser';
 
 export default [
   // browser-friendly UMD build
